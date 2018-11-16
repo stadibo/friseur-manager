9f0c5d4e916c0c5a1d34a064002c2acbbbf4666d
stadibo <jesperleonard@gmail.com>
2018-11-16 16:43:22 +0200

    added authentication functionality

--- application/__init__.py
+++ application/__init__.py
@@ -3,14 +3,43 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friseur.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+# database
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE:URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friseur.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
+# application
 from application import views
 
-from application.users import models
-from application.users import views
+from application.auth import views
+from application.auth import models
 
-db.create_all()
+# login functionality
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please log in to use this functionality."
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# database table creation
+try:
+    db.create_all()
+except:
+    pass
