5992be472267d5ade7f808108fe9aeda190aa36b
stadibo <jesperleonard@gmail.com>
2018-11-25 19:07:49 +0200

    Fix in full appointment data sql stmt

--- application/appointments/models.py
+++ application/appointments/models.py
@@ -32,7 +32,7 @@ class Appointment(Base):
                     "INNER JOIN account_appointment "
                     "ON account_appointment.account_id = :user "
                     "AND account_appointment.appointment_id = appointment.id "
-                    "AND appointment.work_day_id = :work_day "
+                    "WHERE appointment.work_day_id = :work_day "
                     "ORDER BY appointment.time_reserved ASC ;").params(user=user_id, work_day=work_day_id)
         res = db.engine.execute(stmt)
 
@@ -44,18 +44,20 @@ class Appointment(Base):
     
     @staticmethod
     def full_appointment_data():
-        stmt = text("SELECT DISTINCT appointment.time_reserved, appointment.duration, appointment.customer, appointment.reservation_number, account.name, appointment.fulfilled, appointment.id "
+        stmt = text("SELECT DISTINCT appointment.time_reserved, appointment.duration, appointment.customer, appointment.reservation_number, account.name, appointment.fulfilled, appointment.id, work_day.date "
                     "FROM appointment "
-                    "INNER JOIN account_appointment, account, work_day "
+                    "INNER JOIN account_appointment "
+                    "ON account_appointment.appointment_id = appointment.id "
+                    "INNER JOIN account "
                     "ON account_appointment.account_id = account.id "
-                    "AND account.role_id = 2 "
-                    "AND account_appointment.appointment_id = appointment.id "
-                    "AND appointment.work_day_id = work_day.id "
+                    "INNER JOIN work_day "
+                    "ON appointment.work_day_id = work_day.id "
+                    "WHERE account.role_id = 2 "
                     "ORDER BY work_day.date ASC ;")
         res = db.engine.execute(stmt)
 
         response = []
         for row in res:
-            response.append({"time_reserved": row[0], "duration": row[1], "customer": row[2], "reservation_number": row[3], "friseur": row[4], "fulfilled": row[5], "id": row[6]})
+            response.append({"time_reserved": row[0][0:8], "duration": row[1], "customer": row[2], "reservation_number": row[3], "friseur": row[4], "fulfilled": row[5], "id": row[6], "date": row[7][0:10]})
         
         return response
