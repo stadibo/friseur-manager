2fc883dd26ebd678c563e632b9c2d4ff7d4dc771
stadibo <jesperleonard@gmail.com>
2018-12-09 19:25:42 +0200

    Fix sql statement for getting all appointments

--- application/appointments/models.py
+++ application/appointments/models.py
@@ -78,7 +78,7 @@ class Appointment(Base):
                     "ON account_appointment.appointment_id = appointment.id "
                     "LEFT JOIN account "
                     "ON account_appointment.account_id = account.id "
-                    "AND account.role_id == 2 "
+                    "AND account.role_id = 2 "
                     "ORDER BY work_day.date DESC ;")
         res = db.engine.execute(stmt)
 
